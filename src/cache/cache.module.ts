727c2d20a72748c7d27bfa5504b6e6eec55aaa3c
Jacky Rahman <jackyrahman14@gmail.com>
2021-08-06 16:27:53 +0700

    add interceptor

--- src/cache/cache.module.ts
+++ src/cache/cache.module.ts
@@ -1,8 +1,13 @@
-import { CacheModule, Module } from "@nestjs/common";
+import { CacheInterceptor, CacheModule, Module } from "@nestjs/common";
+import { APP_INTERCEPTOR } from "@nestjs/core";
 
 @Module({
-  imports: [CacheModule.register()]
-  // controllers: []
+  imports: [CacheModule.register()],
+  controllers: [],
+  providers: [{
+    provide: APP_INTERCEPTOR,
+    useClass: CacheInterceptor,
+  }]
 })
 
 export class CachingModule {}
