cb60727f43ee8019cc9d781fe84de005abe5644c
Jacky Rahman <jackyrahman14@gmail.com>
2021-08-06 11:26:46 +0700

    need explore for func

---
+++ src/cache/cache.controller.ts
@@ -0,0 +1,7 @@
+import { CacheInterceptor, Controller, UseInterceptors } from "@nestjs/common";
+
+@Controller()
+@UseInterceptors(CacheInterceptor)
+export class CachingController {
+  
+}
--- src/ethereum/ethereum.controller.ts
+++ src/ethereum/ethereum.controller.ts
@@ -2,6 +2,9 @@ import { Controller } from '@nestjs/common';
 import { EthereumService } from './ethereum.service';
 import { EscrowService } from '../escrow/escrow.service';
 import { utils, ethers } from 'ethers';
+import ABI from './utils/ABI.json';
+
+
 import rp from 'request-promise'
 
 @Controller('ethereum')
@@ -31,6 +34,22 @@ export class EthereumController {
         await this.escrowService.handlePaymentToEscrow(from, amount);
       }
     });
+    // const val15 = ethers.utils.formatBytes32String('15')
+    // console.log('decimal 1 to b32 ======> ', val15);
+    // console.log('b 32 ',val15,' to decimmal ======> ', ethers.utils.toUtf8String(val15));
+    const iface = new ethers.utils.Interface(ABI)
+
+    const val = await this.getGasEstimationFee()
+    // console.log('result ', val ,' ======> ', ethers.utils.toUtf8String(val));
+    // console.log('======> ',val,'', ethers.BigNumber.from("0x3135").toString());
+    console.log('iface-->', iface.encodeFunctionData("transferFrom",[
+      "0x42D57aAA086Ee6575Ddd3b502af1b07aEa91E495",
+      "0xd50d4dE65379BAcDFeC59865a0894bc69E020e1D",
+      ethers.utils.parseUnits(String(15), 18)]));
+
+    console.log('======> ',val);
+    
+    
   }
 
   async syncBlock(lastBlock, currentBlock, contract) {
@@ -68,7 +87,23 @@ export class EthereumController {
    
   }
 
-  async getGasEstimationFee(contract){
+  async getGasEstimationFee(){
+    
+    try {
+      const requestOptions = {
+        method: 'POST',
+        uri: 'http://testnet.theapps.dev/rpc',
+        body: {"jsonrpc":"2.0","id":1,"method":"eth_estimateGas","params":[{"data":"0x608060405234801561001057600080fd5b506040516102a13803806102a183398181016040528101906100329190610054565b806000819055505061009e565b60008151905061004e81610087565b92915050565b60006020828403121561006657600080fd5b60006100748482850161003f565b91505092915050565b6000819050919050565b6100908161007d565b811461009b57600080fd5b50565b6101f4806100ad6000396000f3fe608060405234801561001057600080fd5b50600436106100415760003560e01c80637cf5dab0146100465780638381f58a14610062578063d826f88f14610080575b600080fd5b610060600480360381019061005b91906100c5565b61008a565b005b61006a6100a1565b60405161007791906100fd565b60405180910390f35b6100886100a7565b005b806000546100989190610118565b60008190555050565b60005481565b60008081905550565b6000813590506100bf816101a7565b92915050565b6000602082840312156100d757600080fd5b60006100e5848285016100b0565b91505092915050565b6100f78161016e565b82525050565b600060208201905061011260008301846100ee565b92915050565b60006101238261016e565b915061012e8361016e565b9250827fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff0382111561016357610162610178565b5b828201905092915050565b6000819050919050565b7f4e487b7100000000000000000000000000000000000000000000000000000000600052601160045260246000fd5b6101b08161016e565b81146101bb57600080fd5b5056fea2646970667358221220bd81c94057c94a59a3d7a613caf9b6bb9d93827e1c61cfaf444e295e35c2fd8964736f6c634300080000330000000000000000000000000000000000000000000000000000000000000005"}]}
+        ,
+        json: true,
+      };
+      const result =  await rp(requestOptions)
+      return result
+
+    } catch (error) {
+      console.log(error);
+      
+    }
     // console.log("disini------,", await contract.estimateGas.transferFrom(
     //   '0x42D57aAA086Ee6575Ddd3b502af1b07aEa91E495',
     //    '0xd50d4dE65379BAcDFeC59865a0894bc69E020e1D',
@@ -77,34 +112,36 @@ export class EthereumController {
     //   gasLimit: 60000,
     //   gasPrice: ethers.utils.parseUnits('100', 'gwei'),
     // }))
-    const gasPrice = await contract.provider.getGasPrice()
+    // const gasPrice = await contract.provider.getGasPrice()
 
-    return utils.formatUnits(gasPrice, "kwei")
+    // return utils.formatUnits(gasPrice, "kwei")
   }
 
   async convertionCurrency(value, to='USD', from='ETH') {
-    const currencyValue = parseFloat(value)
+    try {
+      const currencyValue = parseFloat(value)
+  
+      const requestOptions = {
+        method: 'GET',
+        uri: 'https://pro-api.coinmarketcap.com/v1/tools/price-conversion',
+        qs: {
+          'amount': value,
+          'symbol': from,
+          'convert': to,
+        },
+        headers: {
+          'X-CMC_PRO_API_KEY': 'fa1134a3-84b3-4ae0-91e4-a3f3b52b05e5'
+        },
+        json: true,
+        gzip: true
+      };
+      const result =  await rp(requestOptions)
+      return result.data
 
-    const requestOptions = {
-      method: 'GET',
-      uri: 'https://pro-api.coinmarketcap.com/v1/tools/price-conversion',
-      qs: {
-        'amount': value,
-        'symbol': from,
-        'convert': to,
-      },
-      headers: {
-        'X-CMC_PRO_API_KEY': 'fa1134a3-84b3-4ae0-91e4-a3f3b52b05e5'
-      },
-      json: true,
-      gzip: true
-    };
-    
-    rp(requestOptions).then(response => {
-      console.log('API call response:',JSON.stringify(response,null,2));
-    }).catch((err) => {
-      console.log('API call error:', err.message);
-    });
+    } catch (error) {
+      console.log(error);      
+    }
+  
   }
   
 }
