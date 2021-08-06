5f622b4b18bce5decf766c3864bc6a13f4ff0868
Jacky Rahman <jackyrahman14@gmail.com>
2021-08-06 11:33:04 +0700

    make funt setOrderRefunded

--- src/ethereum/ethereum.controller.ts
+++ src/ethereum/ethereum.controller.ts
@@ -1,8 +1,6 @@
 import { Controller } from '@nestjs/common';
 import { EthereumService } from './ethereum.service';
 import { EscrowService } from '../escrow/escrow.service';
-import { utils, ethers } from 'ethers';
-import rp from 'request-promise'
 
 @Controller('ethereum')
 export class EthereumController {
@@ -65,46 +63,5 @@ export class EthereumController {
       iStart = iEnd + 1;
       iEnd = iEnd + chunkSize > endBlock ? endBlock : iEnd + chunkSize;
     }
-   
-  }
-
-  async getGasEstimationFee(contract){
-    // console.log("disini------,", await contract.estimateGas.transferFrom(
-    //   '0x42D57aAA086Ee6575Ddd3b502af1b07aEa91E495',
-    //    '0xd50d4dE65379BAcDFeC59865a0894bc69E020e1D',
-    //     1,
-    //     {
-    //   gasLimit: 60000,
-    //   gasPrice: ethers.utils.parseUnits('100', 'gwei'),
-    // }))
-    const gasPrice = await contract.provider.getGasPrice()
-
-    return utils.formatUnits(gasPrice, "kwei")
-  }
-
-  async convertionCurrency(value, to='USD', from='ETH') {
-    const currencyValue = parseFloat(value)
-
-    const requestOptions = {
-      method: 'GET',
-      uri: 'https://pro-api.coinmarketcap.com/v1/tools/price-conversion',
-      qs: {
-        'amount': value,
-        'symbol': from,
-        'convert': to,
-      },
-      headers: {
-        'X-CMC_PRO_API_KEY': 'fa1134a3-84b3-4ae0-91e4-a3f3b52b05e5'
-      },
-      json: true,
-      gzip: true
-    };
-    
-    rp(requestOptions).then(response => {
-      console.log('API call response:',JSON.stringify(response,null,2));
-    }).catch((err) => {
-      console.log('API call error:', err.message);
-    });
-  }
-  
+   }
 }
