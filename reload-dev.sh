236f3bc9dcba7f4be23a74097b95fb295d9bc309
Kevin <kevin.janada@gmail.com>
2021-08-10 10:41:30 +0700

    feat: Update substrate types.json

--- reload-dev.sh
+++ reload-dev.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo "clean up old container ........"
 docker stop debio_backend_v2_dev || true && docker rm debio_backend_v2_dev || true
-docker rmi asia.gcr.io/degenics/debio_backend_v2_dev:latest
-docker pull asia.gcr.io/degenics/debio_backend_v2_dev:latest
+docker rmi hub.debio.network/debio_backend_v2_dev:latest
+docker pull hub.debio.network/debio_backend_v2_dev:latest
 echo "reload container .............."
-docker run -d --network=host --name=debio_backend_v2_dev --env=DEBIOENV=development -v /etc/localtime:/etc/localtime:ro --restart=always asia.gcr.io/degenics/debio_backend_v2_dev:latest
+docker run -d --network=host --name=debio_backend_v2_dev --env=DEBIOENV=development -v /etc/localtime:/etc/localtime:ro --restart=always hub.debio.network/debio_backend_v2_dev:latest
 
