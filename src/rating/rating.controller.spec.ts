0db00f1fa845f17f8486fa64b93e077bc7ea923b
Jacky Rahman <jackyrahman14@gmail.com>
2021-08-25 13:21:30 +0700

    testing controller

---
+++ src/rating/rating.controller.spec.ts
@@ -0,0 +1,24 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { RatingController } from "./rating.controller"
+import { RatingService } from "./rating.service";
+
+describe('Rating Controller', () => {
+  let ratingController: RatingController;
+
+  const mockRatingService = {
+
+  }
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [RatingController],
+      providers: [RatingService],
+    }).overrideProvider(RatingService).useValue(mockRatingService).compile();
+
+    ratingController = module.get<RatingController>(RatingController)
+  });
+
+  it('should be defined', () => {
+    expect(ratingController).toBeDefined();
+  })
+})
