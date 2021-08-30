aee4486273901e5266361dae698ab3734aa83f05
Kevin <kevin.janada@gmail.com>
2021-08-30 21:26:58 +0700

    feat: Wallet Binding via Backend
     ## JIRA LINK
     https://blocksphere2020.atlassian.net/browse/DBIO-362?atlOrigin=eyJpIjoiZDc0MTA1YzUzOWRhNGZhMGIxNTY1N2E2ZGQ3ZWM4NDEiLCJwIjoiaiJ9
    
     ## CHANGELOG
     - Add POST endpoint /wallet-binding to bind eth address to substrate wallet
       - debio-api-key header is required in request
       - Send 1 DBIO to user for the first time they wallet bind
     - Added DEBIO_API_KEY env

--- src/substrate/substrate.controller.ts
+++ src/substrate/substrate.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Res } from '@nestjs/common';
+import { Body, Controller, Post, Res, Headers } from '@nestjs/common';
 import { SubstrateService } from './substrate.service';
 import { Response } from 'express';
 
@@ -7,6 +7,11 @@ export type RegistrationRole = 'lab' | 'doctor' | 'hospital';
 export class GetDbioOnRegisterDto {
   accountId: string;
   role: RegistrationRole;
+}
+
+class WalletBindingDTO {
+  accountId: string;
+  ethAddress: string;
 }
 
 @Controller('substrate')
@@ -45,5 +50,38 @@ export class SubstrateController {
     } catch (err) {
       return response.status(500);
     }
+  }
+
+  @Post('/wallet-binding')
+  async walletBinding(
+    @Body() payload: WalletBindingDTO,
+    @Res() response: Response,
+    @Headers('debio-api-key') debioApiKey: string,
+  ) {
+    if (debioApiKey != process.env.DEBIO_API_KEY) {
+      return response.status(401).send('debio-api-key header is required');
+    }
+
+    const { ethAddress, accountId } = payload;
+
+    const substrateAddress =
+      await this.substrateService.getSubstrateAddressByEthAddress(ethAddress);
+
+    // If user has not bound wallet before, send them 1 DBIO
+    if (substrateAddress == '') {
+      await this.substrateService.sendDbioFromFaucet(
+        accountId,
+        '1000000000000000000',
+      );
+    }
+
+    await this.substrateService.bindEthAddressToSubstrateAddress(
+      ethAddress,
+      accountId,
+    );
+
+    return response
+      .status(200)
+      .send(`eth-address ${ethAddress} bound to ${substrateAddress}`);
   }
 }
