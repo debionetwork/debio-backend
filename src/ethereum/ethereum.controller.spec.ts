87115ed0c492299161d28cd6920721a223cd9ad4
Kristian Ruben <rubenkristian96@gmail.com>
2021-09-11 21:21:10 +0700

    ethereum unit testing and add new setting to jest

---
+++ src/ethereum/ethereum.controller.spec.ts
@@ -0,0 +1,70 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { EscrowService } from "../escrow/escrow.service";
+import { EthereumController } from "./ethereum.controller";
+import { EthereumService } from "./ethereum.service";
+
+describe("EthereumController", () => {
+    let ethereumController: EthereumController;
+    let ethereumService: EthereumService;
+
+    const mockEscrowService = {
+
+    }
+
+    const escrowServiceProvider = {
+        provide: EscrowService,
+        useValue: mockEscrowService
+    }
+
+    const ethereumServiceProvider = {
+        provide: EthereumService,
+        useFactory: () => ({
+            getLastBlock: jest.fn(() => 5484750),
+            setLastBlock: jest.fn(),
+            getContract: jest.fn(() => ({
+                provider: {
+                    getBlockNumber: () => 5484751,
+                    on: () => null,
+                },
+                on: () => null,
+                filters: {
+                    Transfer: () => null
+                }
+            })),
+            createWallet: jest.fn(() => {}),
+            getGasEstimationFee: jest.fn(() => ""),
+            convertCurrency: jest.fn(() => {}),
+        })
+    };
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [EthereumController],
+            providers: [escrowServiceProvider, EthereumService, ethereumServiceProvider]
+        }).compile();
+
+        ethereumController  = module.get<EthereumController>(EthereumController);
+        ethereumService = module.get<EthereumService>(EthereumService);
+    });
+
+    describe("EthereumController", () => {
+        it("EthereumController must defined", () => {
+            expect(ethereumController).toBeDefined();
+        });
+
+
+        it("getContract called", async () => {
+            await ethereumController.onApplicationBootstrap();
+
+            expect(ethereumService.getContract).toBeCalled();
+        })
+
+        it("getLastBlock called", async () => {
+            expect(ethereumService.getLastBlock).toBeCalled();
+        })
+
+        it("setLastBlock called", async () => {
+            expect(ethereumService.setLastBlock).toBeCalled();
+        })
+    });
+});
---
+++ src/ethereum/ethereum.service.spec.ts
@@ -0,0 +1,98 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { EthereumService } from "./ethereum.service";
+
+class EtheretumServiceMock {
+    getLastBlock(): number {
+        return 5484746;
+    }
+
+    setLastBlock(blockNumber: number) {
+        
+    }
+
+    getContract(): any {
+        return {
+            provider: {
+                getBlockNumber: () => 5484748,
+                on: () => null,
+            },
+            on: () => null,
+            filters: {
+                Transfer: () => null
+            }
+        };
+    }
+    
+    createWallet(privateKey: string) {
+        return {};
+    }
+
+    getGasEstimationFee(from, to, data = null) {
+        return "";
+    }
+
+    convertCurrency(fromCurrency = 'ETH', toCurrency = 'DAI', amount: number | string,) {
+        return {};
+    }
+}
+
+describe.only("EthereumService", () => {
+    let ethereumService: EthereumService;
+    
+    beforeAll(async () => {
+        const EthereumServiceProvider = {
+            provide: EthereumService,
+            useClass: EtheretumServiceMock,
+        };
+
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [EthereumService, EthereumServiceProvider]
+        }).compile();
+
+        ethereumService = module.get<EthereumService>(EthereumService);
+    });
+
+    it("should call getLastBlock method with expected params", async() => {
+        const getLastBlockSpy = jest.spyOn(ethereumService, 'getLastBlock');
+        ethereumService.getLastBlock();
+        expect(getLastBlockSpy).toHaveBeenCalledWith();
+    });
+
+    it("should call setLastBlock method with expected params", async() => {
+        const setLastBlockSpy = jest.spyOn(ethereumService, 'setLastBlock');
+        const blockNumber: number = 0;
+        ethereumService.setLastBlock(blockNumber);
+        expect(setLastBlockSpy).toHaveBeenCalledWith(blockNumber);
+    });
+    
+    it("should call getContract method with expected params", async() => {
+        const getContractSpy = jest.spyOn(ethereumService, 'getContract');
+        ethereumService.getContract();
+        expect(getContractSpy).toHaveBeenCalledWith();
+    });
+    
+    it("should call createWallet method with expected params", async() => {
+        const createWalletSpy = jest.spyOn(ethereumService, 'createWallet');
+        const privateKey: string = "";
+        ethereumService.createWallet(privateKey);
+        expect(createWalletSpy).toHaveBeenCalledWith(privateKey);
+    });
+    
+    it("should call getGasEstimationFee method with expected params", async() => {
+        const getGasEstimationFeeSpy = jest.spyOn(ethereumService, 'getGasEstimationFee');
+        const from: string = "";
+        const to: string = "";
+        const data = {};
+        ethereumService.getGasEstimationFee(from, to, data);
+        expect(getGasEstimationFeeSpy).toHaveBeenCalledWith(from, to, data);
+    });
+    
+    it("should call convertCurrency method with expected params", async() => {
+        const convertCurrencySpy = jest.spyOn(ethereumService, 'convertCurrency');
+        const fromCurrency: string = 'ETH';
+        const toCurrency: string = 'DAI';
+        const amount: number | string = 20;
+        ethereumService.convertCurrency(fromCurrency, toCurrency, amount);
+        expect(convertCurrencySpy).toHaveBeenCalledWith(fromCurrency, toCurrency, amount);
+    });
+});
