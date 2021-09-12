76b2cb851e62bf68e35e245af5988fd12576268a
Kirisu <56469224+rubenkristian@users.noreply.github.com>
2021-09-12 13:11:05 +0700

    Check search function in API --> Order History (#51)

---
+++ src/substrate-indexed-data/orders/order.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Param, Query, Req } from '@nestjs/common';
+import { OrderService } from './order.service';
+
+@Controller('orders')
+export class OrderController {
+    constructor(readonly orderService: OrderService) {}
+
+    // host/{customer_id}?query=&page=&size=
+    @Get(':customer_id')
+    async getOrderByProductNameStatusLabName(
+        @Param() params,
+        @Query('keyword') keyword,
+        @Query('page') page,
+        @Query('size') size): Promise<any> {
+        const orders = await this.orderService.getByProductNameStatusLabName(
+            params.customer_id,
+            keyword ? keyword : '',
+            page,
+            size,
+        );
+
+        return orders;
+    }
+}
