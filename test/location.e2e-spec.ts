db6829522ad47641901d917f9c34be46d49f1ebb
Jacky Rahman <jackyrahman14@gmail.com>
2021-09-14 00:21:26 +0700

    fixing location e2e testing

--- test/location.e2e-spec.ts
+++ test/location.e2e-spec.ts
@@ -2,10 +2,11 @@ import { INestApplication } from "@nestjs/common";
 import { Test, TestingModule } from "@nestjs/testing";
 import request from 'supertest';
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { City } from "src/location/models/city.entity";
-import { Country } from "src/location/models/country.entity";
-import { State } from "src/location/models/state.entity";
+import { City } from "../src/location/models/city.entity";
+import { Country } from "../src/location/models/country.entity";
+import { State } from "../src/location/models/state.entity";
 import { LocationModule } from "../src/location/location.module";
+import { Any } from "typeorm";
 
 describe('Rating Controller (e2e)', () => {
   let app: INestApplication;
@@ -15,12 +16,13 @@ describe('Rating Controller (e2e)', () => {
       imports: [
         LocationModule,
         TypeOrmModule.forRoot({
+          name: 'dbLocation',
           type: 'postgres',
           host: process.env.HOST_POSTGRES,
           port: 5432,
           username: process.env.USERNAME_POSTGRES,
           password: process.env.PASSWORD_POSTGRES,
-          database: process.env.DB_POSTGRES,
+          database: process.env.DB_LOCATIONS,
           entities: [City, Country, State],
           autoLoadEntities: true,
         }),
@@ -36,33 +38,74 @@ describe('Rating Controller (e2e)', () => {
     request(app.getHttpServer())
       .get('/location')
       .expect(200)
-      // .then((response) => {
-      //   expect(response.body).toHaveProperty('status')
-      // })
   });
 
-  // it('/rating (GET) by lab_id "3" success get from cache', () => {
-  //   request(app.getHttpServer())
-  //     .get('/rating/3')
-  //     .then((response) => {
-  //       expect(response.body).toEqual({
-  //         status: 'ok',
-  //         data : expect.any(Object)
-  //       })
-  //     })
-  // });
+  it('/location get all state in selected country', () => {
+    request(app.getHttpServer())
+      .get('/location')
+      .query({ country_code : 'ID'})
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toEqual({
+          status : 'ok',
+          data : expect.arrayContaining([{
+            id : expect.any(Number),
+            country_code : expect.any(String),
+            country_id : expect.any(Number),
+            latitude : expect.any(Number),
+            longitude : expect.any(Number),
+            name : expect.any(String),
+            state_code : expect.any(String),
+          }])
+        })
+      })
+  });
 
-  // it('/rating (GET) by lab_id "zzzz" rating will be null', () => {
-  //   request(app.getHttpServer())
-  //     .get('/rating/zzzz')
-  //     .then((response) => {
-  //       expect(response.body).toEqual({
-  //         status: 'ok',
-  //         data : {
-  //           lab_id: 'zzzz',
-  //           rating: null
-  //         }
-  //       })
-  //     })
-  // });
+  it('/location get all city in selected state', () => {
+    request(app.getHttpServer())
+      .get('/location')
+      .query({ 
+        country_code : 'ID',
+        state_code : 'JK'
+      })
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toEqual({
+          status : 'ok',
+          data : expect.arrayContaining([{
+            id : expect.any(Number),
+            country_code : expect.any(String),
+            country_id : expect.any(Number),
+            latitude : expect.any(Number),
+            longitude : expect.any(Number),
+            name : expect.any(String),
+            state_code : expect.any(String),
+            state_id : expect.any(Number),
+          }])
+        })
+      })
+  });
+
+  it('/location get selected city by id', () => {
+    request(app.getHttpServer())
+      .get('/location')
+      .query({ city_id : 56772 })
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toEqual({
+          status : 'ok',
+          data : {
+            id : expect.any(Number),
+            country_code : expect.any(String),
+            country_id : expect.any(Number),
+            latitude : expect.any(Number),
+            longitude : expect.any(Number),
+            name : expect.any(String),
+            state_code : expect.any(String),
+            state_id : expect.any(Number),
+          }
+        })
+      })
+  });
+
 });
