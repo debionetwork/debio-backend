3ca29650a059cd2605227720083be6629368eadc
Jacky Rahman <jackyrahman14@gmail.com>
2021-09-16 14:32:07 +0700

    create connection, module, unit testing end to end

---
+++ src/emr/models/emr.entity.ts
@@ -0,0 +1,10 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({ name: 'emr_category' })
+export class EmrCategory {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  category: string;
+}
