d019a2fce7cd69faa1b61994633f272cc10f8794
rubenkristian <rubenkristian96@gmail.com>
2021-10-04 20:56:20 +0700

    save information of payment history to indexer

--- src/escrow/escrow.module.ts
+++ src/escrow/escrow.module.ts
@@ -5,6 +5,7 @@ import { EscrowController } from './escrow.controller';
 import { EscrowService } from './escrow.service';
 import { SubstrateModule } from '../substrate/substrate.module';
 import { EthereumModule } from '../ethereum/ethereum.module';
+import { ElasticsearchModule } from '@nestjs/elasticsearch';
 
 require('dotenv').config(); // eslint-disable-line
 @Module({
@@ -12,6 +13,11 @@ require('dotenv').config(); // eslint-disable-line
     TypeOrmModule.forFeature([EscrowAccounts]),
     forwardRef(() => SubstrateModule),
     forwardRef(() => EthereumModule),
+    ElasticsearchModule.registerAsync({
+      useFactory: async () => ({
+        node: process.env.ELASTICSEARCH_NODE,
+      }),
+    }),
   ],
   controllers: [EscrowController],
   providers: [EscrowService],
