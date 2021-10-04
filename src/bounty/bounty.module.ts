0f781b4f8591c22b957605c36cbb865a5ba0ba23
rubenkristian <rubenkristian96@gmail.com>
2021-10-04 18:34:04 +0700

    insert hash data bounty after database insert

--- src/bounty/bounty.module.ts
+++ src/bounty/bounty.module.ts
@@ -1,11 +1,19 @@
-import { CacheModule, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { BountyService } from './bounty.service';
 import { BountyController } from './bounty.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataBounty } from './models/bounty.entity';
+import { ElasticsearchModule } from '@nestjs/elasticsearch';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([DataBounty])],
+	imports: [
+		TypeOrmModule.forFeature([DataBounty]),
+    ElasticsearchModule.registerAsync({
+      useFactory: async () => ({
+        node: process.env.ELASTICSEARCH_NODE,
+      }),
+    }),
+	],
   providers: [BountyService],
   controllers: [BountyController]
 })
--- src/bounty/bounty.service.ts
+++ src/bounty/bounty.service.ts
@@ -4,17 +4,31 @@ import { Repository } from 'typeorm';
 import { CreateBountyDto } from './dto/create-bounty.dto';
 import { DataBounty } from './models/bounty.entity';
 import { ethers } from 'ethers';
+import { ElasticsearchService } from '@nestjs/elasticsearch';
 
 @Injectable()
 export class BountyService {
   constructor(
     @InjectRepository(DataBounty)
     private readonly dataBountyRepository: Repository<DataBounty>,
+		private readonly elasticsearchService: ElasticsearchService,
   ) {}
 
-  create(data: CreateBountyDto) {
+  async create(data: CreateBountyDto) {
     const data_bounty = new DataBounty();
 		data_bounty.hash_bounty_ocean = ethers.utils.sha256(ethers.utils.toUtf8Bytes(data.bounty_ocean));
-    return this.dataBountyRepository.save(data_bounty);
+		const save_result = await this.dataBountyRepository.save(data_bounty);
+
+		await this.elasticsearchService.index({
+			index: 'data_bounty',
+			refresh: 'wait_for',
+			id: save_result.id.toString(),
+			body: {
+				id: save_result.id,
+				hash_bounty_ocean: save_result.hash_bounty_ocean
+			},
+		});
+
+    return save_result;
   }
 }
