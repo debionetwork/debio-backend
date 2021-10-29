f9dfe7b4441bd74216a3d19902303ff57939ed25
Jacky Rahman <jackyrahman14@gmail.com>
2021-10-29 15:32:15 +0800

    delete API Post \sendReward\

--- src/reward/reward.module.ts
+++ src/reward/reward.module.ts
@@ -3,7 +3,6 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { DbioBalanceModule } from "src/dbio-balance/dbio_balance.module";
 import { SubstrateModule } from "src/substrate/substrate.module";
 import { Reward } from "./models/reward.entity";
-import { RewardController } from "./reward.controller";
 import { RewardService } from "./reward.service";
 
 @Module({
@@ -12,7 +11,7 @@ import { RewardService } from "./reward.service";
     DbioBalanceModule,
     TypeOrmModule.forFeature([Reward])
   ],
-  controllers: [RewardController],
+  controllers: [],
   providers: [RewardService],
   exports: [TypeOrmModule, RewardService]
 })
