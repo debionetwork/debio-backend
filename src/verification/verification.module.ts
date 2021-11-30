dc9ee31173acbb25a1d1a9930648de18a3d873e4
Agustinus Theodorus <agustinustheoo@gmail.com>
2021-12-01 03:50:37 +0700

    refactor: date time proxy in verification module.

--- src/verification/verification.module.ts
+++ src/verification/verification.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { DateTimeModule } from 'src/common/date-time/date-time.module';
 import { RewardModule } from 'src/reward/reward.module';
 import { SubstrateModule } from 'src/substrate/substrate.module';
 import { VerificationController } from './verification.controller';
 import { VerificationService } from './verification.service';
 
 @Module({
-  imports: [SubstrateModule, RewardModule],
+  imports: [SubstrateModule, RewardModule, DateTimeModule],
   controllers: [VerificationController],
   providers: [VerificationService],
 })
