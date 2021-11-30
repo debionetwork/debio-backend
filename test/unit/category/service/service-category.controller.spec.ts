682f7beef3b03cdc48f909195c92dd07973e5162
Agustinus Theodorus <agustinustheoo@gmail.com>
2021-11-30 23:07:55 +0700

    test: emr and service-category unit tests.

---
+++ test/unit/category/service/service-category.controller.spec.ts
@@ -0,0 +1,47 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ServiceCategoryService } from '../../../../src/category/service/service-category.service';
+import { ServiceCategoryController } from '../../../../src/category/service/service-category.controller';
+import { MockType } from '../../mock';
+
+describe('Service Category Controller Unit Tests', () => {
+  const serviceCategoryServiceMockFactory: () => MockType<ServiceCategoryService> = jest.fn(() => ({
+    getAll: jest.fn(entity => entity),
+  }));
+
+  let serviceCategoryController: ServiceCategoryController;
+  let serviceCategoryServiceMock: MockType<ServiceCategoryService>;
+
+  // Arrange before each iteration
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        ServiceCategoryController,
+        { provide: ServiceCategoryService, useFactory: serviceCategoryServiceMockFactory },
+      ],
+    }).compile();
+    serviceCategoryController = module.get(ServiceCategoryController);
+    serviceCategoryServiceMock = module.get(ServiceCategoryService);
+  });
+
+  it('should be defined', () => {
+    // Assert
+    expect(serviceCategoryController).toBeDefined();
+  });
+
+  it('should find all categories', async () => {
+    // Arrange
+    const categories = [
+        {
+            category: "Example 1"
+        },
+        {
+            category: "Example 2"
+        }
+    ];
+    serviceCategoryServiceMock.getAll.mockReturnValue(categories);
+
+    // Assert
+    expect(serviceCategoryController.getServiceCategory()).toEqual(categories);
+    expect(serviceCategoryServiceMock.getAll).toHaveBeenCalled();
+  });
+});
