2acd9c2e8442d92a4e7ba604995182ead55f4616
Agustinus Theodorus <agustinustheoo@gmail.com>
2021-12-12 06:46:00 +0700

    feat: data token dataset mapping endpoint.

--- src/bounty/bounty.controller.ts
+++ src/bounty/bounty.controller.ts
@@ -1,16 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { ApiBody } from '@nestjs/swagger';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { DataStakingDto } from './dto/data-staking.dto';
 import { DataStakingEvents } from './models/data-staking-events.entity';
 import { DateTimeProxy } from '../common/date-time/date-time.proxy';
+import { DataTokenToDatasetMapping } from './models/data-token-to-dataset-mapping.entity';
 
 @Controller('bounty')
 export class BountyController {
   constructor(
     @InjectRepository(DataStakingEvents)
     private readonly dataStakingEventsRepository: Repository<DataStakingEvents>,
+    @InjectRepository(DataTokenToDatasetMapping)
+    private readonly dataTokenToDatasetMapping: Repository<DataTokenToDatasetMapping>,
     private readonly dateTimeProxy: DateTimeProxy
   ) {}
 
@@ -24,5 +27,12 @@ export class BountyController {
     dataStakingEvents.created_at = new Date(this.dateTimeProxy.now());
     dataStakingEvents.event_processed = false;
     return await this.dataStakingEventsRepository.save(dataStakingEvents);
+  }
+
+  @Get('/staked-files')
+  async StakedFiles(
+    @Query('tokenId') tokenId: string,
+  ) {
+    return await this.dataTokenToDatasetMapping.find({ token_id: tokenId });
   }
 }
--- src/bounty/bounty.module.ts
+++ src/bounty/bounty.module.ts
@@ -3,10 +3,14 @@ import { BountyController } from './bounty.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataStakingEvents } from './models/data-staking-events.entity';
 import { DateTimeModule } from 'src/common/date-time/date-time.module';
+import { DataTokenToDatasetMapping } from './models/data-token-to-dataset-mapping.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([DataStakingEvents]),
+    TypeOrmModule.forFeature([
+      DataStakingEvents, 
+      DataTokenToDatasetMapping
+    ]),
     DateTimeModule,
   ],
   controllers: [BountyController],
