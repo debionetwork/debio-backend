e0b4bdeed8444386dbaab822cbbc024154b27c9b
rubenkristian <rubenkristian96@gmail.com>
2021-12-21 18:50:31 +0700

    import when from jest-when is not used yet

--- test/unit/verification/verification.controller.spec.ts
+++ test/unit/verification/verification.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MockType } from '../mock';
-import { when } from 'jest-when';
+import { _when } from 'jest-when';
 import { VerificationService } from '../../../src/verification/verification.service';
 import { VerificationController } from '../../../src/verification/verification.controller';
 import { ProcessEnvProxy } from '../../../src/common/process-env';
