33c9d60d94fea0775b89d457ce69cc58b5290a52
Agustinus Theodorus <37565691+agustinustheo@users.noreply.github.com>
2021-12-23 12:52:08 +0700

    feat: mailer scheduler for future update. (#189)

---
+++ src/schedulers/mailer/mailer.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
+import { CustomerStakingRequestService, MailerManager } from '../../common';
+
+@Injectable()
+export class MailerService {
+  private logger : Logger = new Logger(MailerService.name)
+  constructor(
+    private readonly mailerManager: MailerManager
+  ) {}
+
+  // TODO: Mailer must use a scheduler to counterract 
+  // failed mail requests from the frontend application
+  @Cron("0 0 0 1 12 *")
+  async handleCron() {
+      // Get recipients here
+      let recipients: string[] = [];
+
+      for (const recipient of recipients) {
+          await this.mailerManager.sendCustomerStakingRequestServiceEmail(
+              recipient,
+              new CustomerStakingRequestService()
+          );
+      }
+  }
+}
