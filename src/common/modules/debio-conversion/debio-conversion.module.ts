c58d4103d822401d68628f926dbb17e5ee825b0f
Agustinus Theodorus <37565691+agustinustheo@users.noreply.github.com>
2022-01-11 23:42:26 +0700

    tests: debio conversion service unit tests. (#213)
    * refactor: move mailer tests.
    
    * refactor: debio conversion module.
    
    * tests: debio conversion service unit tests.
    
    * tests: fix unit tests.

--- src/common/modules/debio-conversion/debio-conversion.module.ts
+++ src/common/modules/debio-conversion/debio-conversion.module.ts
@@ -1,7 +1,13 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { ProcessEnvModule } from '../../proxies/process-env';
 import { DebioConversionService } from './debio-conversion.service';
 
 @Module({
+  imports: [
+    ProcessEnvModule,
+    HttpModule,
+  ],
   providers: [DebioConversionService],
   exports: [DebioConversionService],
 })
