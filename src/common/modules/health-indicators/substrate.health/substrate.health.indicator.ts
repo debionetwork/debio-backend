e60efce81843d2ef07b4712693aa44f5525bebce
Agustinus Theodorus <agustinustheoo@gmail.com>
2022-01-21 04:14:04 +0700

    tests: substrate health indicator.

--- src/common/modules/health-indicators/substrate.health/substrate.health.indicator.ts
+++ src/common/modules/health-indicators/substrate.health/substrate.health.indicator.ts
@@ -18,13 +18,12 @@ export class SubstrateHealthIndicator extends HealthIndicator {
       if (!conn) {
         throw Error('Substrate Node Disconnected');
       }
+      return this.getStatus('substrate-node', true);
     } catch (error) {
       throw new HealthCheckError(
         'SubstrateHealthIndicator failed',
-        this.getStatus('substrate-node', false),
+        error,
       );
     }
-
-    return this.getStatus('substrate-node', true);
   }
 }
