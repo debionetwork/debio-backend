4f8f22bde8586c423411150591a9685edf7bfb26
Agustinus Theodorus <agustinustheoo@gmail.com>
2022-01-21 23:23:47 +0700

    tests: health controller unit tests.

--- src/endpoints/health/health.module.ts
+++ src/endpoints/health/health.module.ts
@@ -4,7 +4,11 @@ import { TerminusModule } from '@nestjs/terminus';
 import { ElasticsearchHealthModule, SubstrateHealthModule } from '../../common';
 
 @Module({
-  imports: [TerminusModule, ElasticsearchHealthModule, SubstrateHealthModule],
+  imports: [
+    TerminusModule, 
+    ElasticsearchHealthModule, 
+    SubstrateHealthModule
+  ],
   controllers: [HealthController],
 })
 export class HealthModule {}
