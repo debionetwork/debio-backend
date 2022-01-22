dfa7c5a3304f850544a8cd2f8da14a11eb6d4cb3
Agustinus Theodorus <agustinustheoo@gmail.com>
2022-01-23 01:24:04 +0700

    tests: substrate service unit tests.

---
+++ test/unit/common/modules/substrate/substrate.service.spec.ts
@@ -0,0 +1,98 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { mockFunction } from '../../../mock';
+import { ProcessEnvProxy, SubstrateService } from '../../../../../src/common';
+import { ApiPromise, Keyring, WsProvider } from '@polkadot/api';
+
+jest.mock('../../../mock', () => ({
+    mockFunction: jest.fn(),
+}));
+
+const apiPromiseSpy = jest.spyOn(ApiPromise, 'create');
+const keyringSpy = jest.spyOn(Keyring.prototype, 'addFromUri');
+
+describe.only('Substrate Service Unit Test', () => {
+  let substrateService: SubstrateService;
+
+  const SUBSTRATE_URL = "URL";
+  const ADMIN_SUBSTRATE_MNEMONIC = "ADDR";
+  class ProcessEnvProxyMock { 
+    env = {
+      SUBSTRATE_URL,
+      ADMIN_SUBSTRATE_MNEMONIC,
+    }
+   };
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        SubstrateService,
+        { provide: ProcessEnvProxy, useClass: ProcessEnvProxyMock },
+      ],
+    }).compile();
+
+    substrateService = module.get(SubstrateService);
+  });
+
+  it('should be defined', () => {
+    // Assert
+    expect(substrateService).toBeDefined();
+  });
+
+  it('should initialize module and listener', async () => {
+    // Reset Everything
+    await substrateService.stopListen();
+    (mockFunction as jest.Mock).mockClear();
+    apiPromiseSpy.mockClear();
+    keyringSpy.mockClear();
+    
+    // Act
+    await substrateService.onModuleInit();
+    
+    // Assert
+    expect(mockFunction).toHaveBeenCalledTimes(5);
+    expect(mockFunction).toHaveBeenCalledWith(SUBSTRATE_URL);
+    expect(mockFunction).toHaveBeenCalledWith({ type: 'sr25519' });
+    expect(mockFunction).toHaveBeenCalledWith('connected');
+    expect(mockFunction).toHaveBeenCalledWith('disconnected');
+    expect(mockFunction).toHaveBeenCalledWith('error');
+    expect(keyringSpy).toHaveBeenCalledTimes(1);
+    expect(keyringSpy).toHaveBeenCalledWith(ADMIN_SUBSTRATE_MNEMONIC);
+    expect(apiPromiseSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should initialize listener', async () => {
+    // Reset Everything
+    await substrateService.stopListen();
+    (mockFunction as jest.Mock).mockClear();
+    apiPromiseSpy.mockClear();
+
+    // Act
+    await substrateService.startListen();
+    
+    // Assert
+    expect(apiPromiseSpy).toHaveBeenCalledTimes(1);
+    expect(mockFunction).toHaveBeenCalledTimes(3);
+    expect(mockFunction).toHaveBeenCalledWith('connected');
+    expect(mockFunction).toHaveBeenCalledWith('disconnected');
+    expect(mockFunction).toHaveBeenCalledWith('error');
+  });
+
+  it('should stop listener and start again', async () => {
+    // Reset Everything
+    await substrateService.stopListen();
+    (mockFunction as jest.Mock).mockClear();
+    apiPromiseSpy.mockClear();
+
+    // Act
+    await substrateService.startListen();
+    await substrateService.stopListen();
+    await substrateService.startListen();
+    
+    // Assert
+    expect(apiPromiseSpy).toHaveBeenCalledTimes(2);
+    expect(mockFunction).toHaveBeenCalledTimes(6);
+    expect(mockFunction).toHaveBeenCalledWith('connected');
+    expect(mockFunction).toHaveBeenCalledWith('disconnected');
+    expect(mockFunction).toHaveBeenCalledWith('error');
+  });
+});
