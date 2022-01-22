3183f56917f60ee61eade2936d2535880ce26231
Agustinus Theodorus <agustinustheoo@gmail.com>
2022-01-23 04:32:51 +0700

    tests: mock polkadot data and class.

--- test/unit/listeners/substrate-listener/substrate-listener.mock.data.ts
+++ test/unit/listeners/substrate-listener/substrate-listener.mock.data.ts
@@ -1,3 +1,5 @@
+import { Event as PolkadotTypesEvent } from '../../@polkadot-types-interfaces.mock';
+
 export const ERROR_MOCK = {
     name: "NAME",
     message: "MESSAGE",
@@ -27,23 +29,61 @@ export const CATCH_MOCK = {
         fn3(ERROR_MOCK);
     }
 };
+
+let arr = [];
 export const API_MOCK = {
-    rpc: {
-      chain: {
-        getBlock: () => {
-          return BLOCK_MOCK
-        },
-        getBlockHash: number => { // eslint-disable-line
-          return BLOCK_HASH_MOCK
-        },
+  rpc: {
+    chain: {
+      getBlock: () => {
+        return BLOCK_MOCK
+      },
+      getBlockHash: number => { // eslint-disable-line
+        return BLOCK_HASH_MOCK
       },
     },
-    query: {
-      system: {
-        events: fn => {
-          fn();
-          return THEN_MOCK;
-        }
+  },
+  query: {
+    system: {
+      events: fn => {
+        fn(arr);
+        arr = []; // Reset array
+        return THEN_MOCK;
       }
     }
+  }
 };
+export const API_MOCK_GEN = (_arr: Array<any>) => {
+  arr = _arr;
+  return API_MOCK;
+};
+export const TO_HUMAN_MOCK = {
+  toHuman: jest.fn(() => {
+    return {
+      id: "string",
+      serviceId: "string",
+      customerId: "string",
+      customerBoxPublicKey: "string",
+      sellerId: "string",
+      dnaSampleTrackingId: "string",
+      currency: "CurrencyType" as any,
+      prices: "Price[]" as any,
+      additionalPrices: "Price[]" as any,
+      status: "OrderStatus" as any,
+      orderFlow: "ServiceFlow" as any,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+  }),
+};
+export const EVENT_MOCK: PolkadotTypesEvent = {
+  data: [
+    TO_HUMAN_MOCK,
+  ],
+  section: "orders",
+  method: "OrderCreated",
+}
+export const FALSE_EVENT_MOCK: PolkadotTypesEvent = {
+  data: ["DATA"],
+  section: "SECTION",
+  method: "METHOD",
+}
