4e71718d7fe1afe84877756beecd055f63c02f2f
unknown <jackyrahman14@gmail.com>
2022-01-24 16:03:03 +0700

    tests: set system fake time for test

--- test/unit/common/modules/proxies/date-time.proxy.spec.ts
+++ test/unit/common/modules/proxies/date-time.proxy.spec.ts
@@ -24,18 +24,27 @@ describe('Date Time Proxy Unit Tests', () => {
   });
 
   it('should be get new date', async () => {
+    jest
+      .useFakeTimers()
+      .setSystemTime(new Date('2022-01-24').getTime());
     const ES_RESULT = new Date();
     const RESULT = await dateTimeProxyMock.new()
     expect(RESULT).toEqual(ES_RESULT);
   })
 
   it('should be get now date', async () => {
+    jest
+    .useFakeTimers()
+    .setSystemTime(new Date('2022-01-24').getTime());
     const ES_RESULT = Date.now();
     const RESULT = await dateTimeProxyMock.now()
     expect(RESULT).toEqual(ES_RESULT);
   })
 
   it('should be get now and add ', async () => {
+    jest
+    .useFakeTimers()
+    .setSystemTime(new Date('2022-01-24').getTime());
     const DATE_WHEN_TEST = Number(Date.now())
     const RESULT = await dateTimeProxyMock.nowAndAdd(1)
     expect(RESULT).toEqual(DATE_WHEN_TEST+1);
