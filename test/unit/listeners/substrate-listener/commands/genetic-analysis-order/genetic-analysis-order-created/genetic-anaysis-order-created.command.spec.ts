bca1b27017eb06538e53ef0f9643a6eb83e49c42
unknown <jackyrahman14@gmail.com>
2022-02-17 02:55:39 +0700

    tests: create test for GA order listener created and paid

---
+++ test/unit/listeners/substrate-listener/commands/genetic-analysis-order/genetic-analysis-order-created/genetic-anaysis-order-created.command.spec.ts
@@ -0,0 +1,29 @@
+import { GeneticAnalysisOrderStatus } from '../../../../../../../src/common';
+import { GeneticAnalysisOrderCreatedCommand } from '../../../../../../../src/listeners/substrate-listener/commands/genetic-analysis-order';
+import { createMockGeneticAnalysisOrder, mockBlockNumber } from '../../../../../mock';
+import { GeneticAnalystOrder } from '../../../../../../../src/common/polkadot-provider/models/genetic-analysis-orders';
+
+jest.mock('../../../../../../../src/common/polkadot-provider/models/genetic-analysis-orders');
+
+describe('Genetic Analysis Order Created Command Event', () => {
+  it('should called model data and toHuman', () => {
+    const GA_ORDER_RESPONSE = createMockGeneticAnalysisOrder(GeneticAnalysisOrderStatus.Unpaid);
+
+    const _orderCreatedCommand: GeneticAnalysisOrderCreatedCommand = new GeneticAnalysisOrderCreatedCommand(
+      [GA_ORDER_RESPONSE],
+      mockBlockNumber(),
+    );
+    expect(GeneticAnalystOrder).toHaveBeenCalled();
+    expect(GeneticAnalystOrder).toHaveBeenCalledWith(GA_ORDER_RESPONSE.toHuman());
+    expect(GA_ORDER_RESPONSE.toHuman).toHaveBeenCalled();
+  });
+
+  it('should throw error if toHuman not defined', () => {
+    expect(() => {
+      const _orderCreatedCommand: GeneticAnalysisOrderCreatedCommand = new GeneticAnalysisOrderCreatedCommand(
+        [{}],
+        mockBlockNumber(),
+      );
+    }).toThrowError();
+  });
+});
