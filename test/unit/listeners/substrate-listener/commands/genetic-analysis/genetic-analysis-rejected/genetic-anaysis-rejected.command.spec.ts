bccc318bdf116f37e029d66dda63c754786bdef8
unknown <jackyrahman14@gmail.com>
2022-02-18 05:34:44 +0700

    tests: create unit test for GA rejected

---
+++ test/unit/listeners/substrate-listener/commands/genetic-analysis/genetic-analysis-rejected/genetic-anaysis-rejected.command.spec.ts
@@ -0,0 +1,25 @@
+import { GeneticAnalysisStatus } from '../../../../../../../src/common';
+import { GeneticAnalysisRejectedCommand } from '../../../../../../../src/listeners/substrate-listener/commands/genetic-analysis';
+import { createMockGeneticAnalysis, mockBlockNumber } from '../../../../../mock';
+import { GeneticAnalysis } from '../../../../../../../src/common/polkadot-provider/models/genetic-analisys';
+
+jest.mock('../../../../../../../src/common/polkadot-provider/models/genetic-analisys');
+
+describe('Genetic Analysis Order Paid Command Event', () => {
+  it('should called model data and toHuman', () => {
+    const GA_ORDER_RESPONSE = createMockGeneticAnalysis(GeneticAnalysisStatus.Rejected);
+
+    const _= // eslint-disable-line
+      new GeneticAnalysisRejectedCommand([GA_ORDER_RESPONSE], mockBlockNumber());
+    expect(GeneticAnalysis).toHaveBeenCalled();
+    expect(GeneticAnalysis).toHaveBeenCalledWith(GA_ORDER_RESPONSE.toHuman());
+    expect(GA_ORDER_RESPONSE.toHuman).toHaveBeenCalled();
+  });
+
+  it('should throw error if toHuman not defined', () => {
+    expect(() => {
+      const _= // eslint-disable-line
+        new GeneticAnalysisRejectedCommand([{}], mockBlockNumber());
+    }).toThrowError();
+  });
+});
