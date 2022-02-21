cca5145568d4f530021924d0d5bde44aaa7a94b2
unknown <jackyrahman14@gmail.com>
2022-02-22 06:06:37 +0700

    tests: create GA verification status

---
+++ test/unit/listeners/substrate-listener/commands/genetic-analyst/genetic-analyst-verification-status/genetic-analyst-verification-status.handler.spec.ts
@@ -0,0 +1,178 @@
+import {
+  DateTimeProxy,
+  TransactionLoggingService,
+} from '../../../../../../../src/common';
+import { GeneticAnalystVerificationStatusCommand } from '../../../../../../../src/listeners/substrate-listener/commands/genetic-analysts';
+import { Test, TestingModule } from '@nestjs/testing';
+import {
+  createMockGeneticAnalyst,
+  mockBlockNumber,
+  MockType,
+  dateTimeProxyMockFactory,
+  transactionLoggingServiceMockFactory,
+} from '../../../../../mock';
+import { GeneticAnalystVerificationStatusHandler } from '../../../../../../../src/listeners/substrate-listener/commands/genetic-analysts/genetic-analyst-verification-status/genetic-analyst-verification-status.handler';
+import { when } from 'jest-when';
+import { TransactionRequest } from '../../../../../../../src/common/modules/transaction-logging/models/transaction-request.entity';
+
+describe('Genetic Analyst Verification Status Handler Event', () => {
+  let geneticAnalystVerificationStatusHandler: GeneticAnalystVerificationStatusHandler;
+  let transactionLoggingServiceMock: MockType<TransactionLoggingService>;
+  let dateTimeProxyMock: MockType<DateTimeProxy>;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: TransactionLoggingService,
+          useFactory: transactionLoggingServiceMockFactory,
+        },
+        { 
+          provide: DateTimeProxy, useFactory: dateTimeProxyMockFactory 
+        },
+        GeneticAnalystVerificationStatusHandler,
+      ],
+    }).compile();
+
+    geneticAnalystVerificationStatusHandler = module.get(
+      GeneticAnalystVerificationStatusHandler,
+    );
+    transactionLoggingServiceMock = module.get(TransactionLoggingService);
+
+    await module.init();
+  });
+
+  it('should defined Genetic Analyst Verification Status Handler', () => {
+    expect(geneticAnalystVerificationStatusHandler).toBeDefined();
+  });
+
+  it('should not called logging Verification Status', async () => {
+    // Arrange
+    const geneticAnalyst = createMockGeneticAnalyst();
+
+    const RESULT_STATUS = true;
+    const RESULT_TRANSACTION: TransactionRequest = new TransactionRequest();
+    RESULT_TRANSACTION.id = BigInt(0);
+    RESULT_TRANSACTION.address = 'string';
+    RESULT_TRANSACTION.amount = 0;
+    RESULT_TRANSACTION.created_at = new Date();
+    RESULT_TRANSACTION.currency = 'string';
+    RESULT_TRANSACTION.parent_id = BigInt(0);
+    RESULT_TRANSACTION.ref_number = 'string';
+    RESULT_TRANSACTION.transaction_type = 0;
+    RESULT_TRANSACTION.transaction_status = 0;
+    RESULT_TRANSACTION.transaction_hash = 'string';
+
+    when(transactionLoggingServiceMock.getLoggingByHashAndStatus)
+      .calledWith(geneticAnalyst.toHuman().accountId, 14)
+      .mockReturnValue(RESULT_STATUS);
+
+    const geneticAnalystOrders: GeneticAnalystVerificationStatusCommand =
+      new GeneticAnalystVerificationStatusCommand([geneticAnalyst], mockBlockNumber());
+
+    await geneticAnalystVerificationStatusHandler.execute(geneticAnalystOrders);
+    expect(
+      transactionLoggingServiceMock.getLoggingByHashAndStatus,
+    ).toHaveBeenCalled();
+    expect(transactionLoggingServiceMock.create).not.toHaveBeenCalled();
+  });
+
+  it('should called logging Verification Status Rejected', async () => {
+    // Arrange
+    const geneticAnalyst = createMockGeneticAnalyst();
+    geneticAnalyst.toHuman().verificationStatus = 'Rejected'
+
+    const RESULT_STATUS = { id: 1 };
+    const RESULT_TRANSACTION: TransactionRequest = new TransactionRequest();
+    RESULT_TRANSACTION.id = BigInt(0);
+    RESULT_TRANSACTION.address = 'string';
+    RESULT_TRANSACTION.amount = 0;
+    RESULT_TRANSACTION.created_at = new Date();
+    RESULT_TRANSACTION.currency = 'string';
+    RESULT_TRANSACTION.parent_id = BigInt(1);
+    RESULT_TRANSACTION.ref_number = 'string';
+    RESULT_TRANSACTION.transaction_type = 0;
+    RESULT_TRANSACTION.transaction_status = 0;
+    RESULT_TRANSACTION.transaction_hash = 'string';
+
+    when(transactionLoggingServiceMock.getLoggingByHashAndStatus)
+      .calledWith(geneticAnalyst.toHuman().accountId, 14)
+      .mockReturnValue(RESULT_STATUS);
+
+    const geneticAnalystVerificationStatusCommand: GeneticAnalystVerificationStatusCommand =
+      new GeneticAnalystVerificationStatusCommand([geneticAnalyst], mockBlockNumber());
+
+    await geneticAnalystVerificationStatusHandler.execute(
+      geneticAnalystVerificationStatusCommand,
+    );
+    expect(
+      transactionLoggingServiceMock.getLoggingByHashAndStatus,
+    ).toHaveBeenCalled();
+  });
+
+  it('should called logging Verification Status Verified', async () => {
+    // Arrange
+    const geneticAnalyst = createMockGeneticAnalyst();
+    geneticAnalyst.toHuman().verificationStatus = 'Verified'
+
+    const RESULT_STATUS = { id: 1 };
+    const RESULT_TRANSACTION: TransactionRequest = new TransactionRequest();
+    RESULT_TRANSACTION.id = BigInt(0);
+    RESULT_TRANSACTION.address = 'string';
+    RESULT_TRANSACTION.amount = 0;
+    RESULT_TRANSACTION.created_at = new Date();
+    RESULT_TRANSACTION.currency = 'string';
+    RESULT_TRANSACTION.parent_id = BigInt(1);
+    RESULT_TRANSACTION.ref_number = 'string';
+    RESULT_TRANSACTION.transaction_type = 0;
+    RESULT_TRANSACTION.transaction_status = 0;
+    RESULT_TRANSACTION.transaction_hash = 'string';
+
+    when(transactionLoggingServiceMock.getLoggingByHashAndStatus)
+      .calledWith(geneticAnalyst.toHuman().accountId, 14)
+      .mockReturnValue(RESULT_STATUS);
+
+    const geneticAnalystVerificationStatusCommand: GeneticAnalystVerificationStatusCommand =
+      new GeneticAnalystVerificationStatusCommand([geneticAnalyst], mockBlockNumber());
+
+    await geneticAnalystVerificationStatusHandler.execute(
+      geneticAnalystVerificationStatusCommand,
+    );
+    expect(
+      transactionLoggingServiceMock.getLoggingByHashAndStatus,
+    ).toHaveBeenCalled();
+  })
+
+  it('should called logging Verification Status Revoked', async () => {
+    // Arrange
+    const geneticAnalyst = createMockGeneticAnalyst();
+    geneticAnalyst.toHuman().verificationStatus = 'Revoked'
+
+    const RESULT_STATUS = { id: 1 };
+    const RESULT_TRANSACTION: TransactionRequest = new TransactionRequest();
+    RESULT_TRANSACTION.id = BigInt(0);
+    RESULT_TRANSACTION.address = 'string';
+    RESULT_TRANSACTION.amount = 0;
+    RESULT_TRANSACTION.created_at = new Date();
+    RESULT_TRANSACTION.currency = 'string';
+    RESULT_TRANSACTION.parent_id = BigInt(1);
+    RESULT_TRANSACTION.ref_number = 'string';
+    RESULT_TRANSACTION.transaction_type = 0;
+    RESULT_TRANSACTION.transaction_status = 0;
+    RESULT_TRANSACTION.transaction_hash = 'string';
+
+    when(transactionLoggingServiceMock.getLoggingByHashAndStatus)
+      .calledWith(geneticAnalyst.toHuman().accountId, 14)
+      .mockReturnValue(RESULT_STATUS);
+
+    const geneticAnalystVerificationStatusCommand: GeneticAnalystVerificationStatusCommand =
+      new GeneticAnalystVerificationStatusCommand([geneticAnalyst], mockBlockNumber());
+
+    await geneticAnalystVerificationStatusHandler.execute(
+      geneticAnalystVerificationStatusCommand,
+    );
+    expect(
+      transactionLoggingServiceMock.getLoggingByHashAndStatus,
+    ).toHaveBeenCalled();
+  })
+});
---
+++ test/unit/listeners/substrate-listener/commands/genetic-analyst/genetic-analyst-verification-status/genetic-anayst-verification-status.command.spec.ts
@@ -0,0 +1,31 @@
+import { GeneticAnalystVerificationStatusCommand } from '../../../../../../../src/listeners/substrate-listener/commands/genetic-analysts';
+import {
+  createMockGeneticAnalyst,
+  mockBlockNumber,
+} from '../../../../../mock';
+import { GeneticAnalyst } from '../../../../../../../src/common/polkadot-provider/models/genetic-analysts';
+
+jest.mock(
+  '../../../../../../../src/common/polkadot-provider/models/genetic-analysts',
+);
+
+describe('Genetic Analyst Verification Status Command Event', () => {
+  it('should called model data and toHuman', () => {
+    const GA_RESPONSE = createMockGeneticAnalyst();
+
+    const _= // eslint-disable-line
+      new GeneticAnalystVerificationStatusCommand([GA_RESPONSE], mockBlockNumber());
+    expect(GeneticAnalyst).toHaveBeenCalled();
+    expect(GeneticAnalyst).toHaveBeenCalledWith(
+      GA_RESPONSE.toHuman(),
+    );
+    expect(GA_RESPONSE.toHuman).toHaveBeenCalled();
+  });
+
+  it('should throw error if toHuman not defined', () => {
+    expect(() => {
+      const _= // eslint-disable-line
+        new GeneticAnalystVerificationStatusCommand([{}], mockBlockNumber());
+    }).toThrowError();
+  });
+});
