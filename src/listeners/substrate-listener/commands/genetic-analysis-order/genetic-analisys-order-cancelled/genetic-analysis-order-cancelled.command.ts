b62f5f804900cf494a5fa98fd7bf63a677aa7e1a
unknown <jackyrahman14@gmail.com>
2022-03-18 07:55:44 +0700

    feat: draft genetic analysis order cancelled

--- src/listeners/substrate-listener/commands/genetic-analysis-order/genetic-analisys-order-cancelled/genetic-analysis-order-cancelled.command.ts
+++ src/listeners/substrate-listener/commands/genetic-analysis-order/genetic-analisys-order-cancelled/genetic-analysis-order-cancelled.command.ts
@@ -1,7 +1,7 @@
 import { BlockMetaData } from '../../../models/block-metadata.event-model';
 import { GeneticAnalysisOrder } from '@debionetwork/polkadot-provider';
 
-export class GeneticAnalysisOrderFulfilledCommand {
+export class GeneticAnalysisOrderCancelledCommand {
   geneticAnalysisOrders: GeneticAnalysisOrder;
   constructor(data: Array<any>, public readonly blockMetaData: BlockMetaData) {
     const geneticAnalysisOrderData = data[0];
