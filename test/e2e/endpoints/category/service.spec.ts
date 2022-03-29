f085cfa0162f795d1d9fb31af2b1fb60b4d9f551
JackyRahman <78291196+JackyRahman@users.noreply.github.com>
2022-03-29 13:23:54 +0700

    tests: category endpoint integration tests (#301)
    * re new branch
    
    * big fixing API substrate/customer/:customer_id
    
    * fixing endpoint and unit testing
    
    * upgrate package to 0.0.4
    
    * run githooks
    
    * tests: category endpoint
    
    * run git hooks

---
+++ test/e2e/endpoints/category/service.spec.ts
@@ -0,0 +1,37 @@
+import { INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Server } from 'http';
+import { ServiceCategory } from 'src/endpoints/category/service/models/service-category.service';
+import { dummyCredentials } from 'test/e2e/config';
+import { ServiceCategoryModule } from 'src/endpoints/category/service/service-category.module';
+
+describe('Service Category (e2e)', () => {
+  let server: Server;
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          ...dummyCredentials,
+          name: 'dbLocation',
+          database: 'db_location',
+          entities: [ServiceCategory],
+          autoLoadEntities: true,
+        }),
+        ServiceCategoryModule,
+      ],
+    }).compile();
+    app = module.createNestApplication();
+    server = app.getHttpServer();
+    await app.init();
+  });
+
+  it('GET /service-category', async () => {
+    //Act
+    const result = await request(server).get('/service-category').send();
+    expect(result.status).toEqual(200);
+  }, 25000);
+});
