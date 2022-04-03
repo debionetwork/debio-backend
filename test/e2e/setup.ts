fc1e34d7d7346d769ab4a4ce8c2705cda15455d9
Agustinus Theodorus <agustinustheoo@gmail.com>
2022-04-03 18:17:44 +0700

    tests: ci

--- test/e2e/setup.ts
+++ test/e2e/setup.ts
@@ -14,6 +14,7 @@ module.exports = async () => {
       () => {
         fixture()
           .then(() => {
+            console.log("HI");
             substrateFixture()
               .then(() => {
                 elasticsearchFixture()
