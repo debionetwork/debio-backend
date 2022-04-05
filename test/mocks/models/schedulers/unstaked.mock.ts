e40f89025af99bdbfae13c1ec59781192deced94
rubenkristian <rubenkristian96@gmail.com>
2022-04-05 23:22:15 +0700

    integration test registered

--- test/mocks/models/schedulers/unstaked.mock.ts
+++ test/mocks/models/schedulers/unstaked.mock.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // eslint-disable-next-line
 const serviceRequestMockData = {
   request: {
@@ -15,3 +16,21 @@ const serviceRequestMockData = {
     unstaked_at: new Date(),
   },
 };
+=======
+const serviceRequestMockData = {
+  request: {
+    hash: "0x22d03ae32b4c2dcca4f69c26f768aa160d9466f501bfb622e6d84c639e621fa9",
+    requester_address: "5GH6Kqaz3ZewWvDCZPkTnsRezUf2Q7zZ5GmC4XFLNqKdVwA7",
+    lab_address: "5GH6Kqaz3ZewWvDCZPkTnsRezUf2Q7zZ5GmC4XFLNqKdVwA7",
+    country: "ID",
+    city: "Kota Administrasi Jakarta Barat",
+    region: "JK",
+    service_category: "Single Gene",
+    staking_amount: "1,000,000,000,000,000,000",
+    status: "WaitingForUnstaked",
+    created_at: "1,648,719,648,000",
+    updated_at: null,
+    unstaked_at: new Date()
+  }
+}
+>>>>>>> integration test registered
