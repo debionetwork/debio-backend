3b7a75cb28d3a41f0603cba0748160c34527e6a2
rubenkristian <rubenkristian96@gmail.com>
2022-04-10 23:14:40 +0700

    lint fixed

--- test/e2e/schedulers/unstaked.spec.ts
+++ test/e2e/schedulers/unstaked.spec.ts
@@ -7,7 +7,10 @@ import {
   SubstrateModule,
   SubstrateService,
 } from '../../../src/common';
-import { ElasticsearchModule, ElasticsearchService } from '@nestjs/elasticsearch';
+import {
+  ElasticsearchModule,
+  ElasticsearchService,
+} from '@nestjs/elasticsearch';
 import { ScheduleModule, SchedulerRegistry } from '@nestjs/schedule';
 import { UnstakedService } from '../../../src/schedulers/unstaked/unstaked.service';
 import * as serviceRequestQuery from '@debionetwork/polkadot-provider/lib/query/service-request';
@@ -54,8 +57,13 @@ describe('Unstaked Scheduler (e2e)', () => {
     substrateService = module.get(SubstrateService);
     processEnvProxy = module.get(ProcessEnvProxy);
     elasticsearchService = module.get(ElasticsearchService);
-    
-    unstakedService = new UnstakedService(processEnvProxy, elasticsearchService, substrateService, schedulerRegistry);
+
+    unstakedService = new UnstakedService(
+      processEnvProxy,
+      elasticsearchService,
+      substrateService,
+      schedulerRegistry,
+    );
 
     app = module.createNestApplication();
     await app.init();
@@ -69,7 +77,9 @@ describe('Unstaked Scheduler (e2e)', () => {
   it('unstaked-interval must registered', async () => {
     unstakedService.onModuleInit();
 
-    expect(schedulerRegistry.doesExists('interval', 'unstaked-interval')).toBeTruthy();
+    expect(
+      schedulerRegistry.doesExists('interval', 'unstaked-interval'),
+    ).toBeTruthy();
 
     clearInterval(schedulerRegistry.getInterval('unstaked-interval'));
   });
@@ -81,7 +91,8 @@ describe('Unstaked Scheduler (e2e)', () => {
       'queryServiceRequestById',
     );
     const RESULT: ServiceRequest = new ServiceRequest({
-      hash_: '0x8b48ead7cf44e6449cbb5de298f3c3915f09b700b7a74b27a368c69629884155',
+      hash_:
+        '0x8b48ead7cf44e6449cbb5de298f3c3915f09b700b7a74b27a368c69629884155',
       requesterAddress: '5GH6Kqaz3ZewWvDCZPkTnsRezUf2Q7zZ5GmC4XFLNqKdVwA7',
       labAddress: null,
       country: 'ID',
@@ -94,8 +105,8 @@ describe('Unstaked Scheduler (e2e)', () => {
       updatedAt: '1,648,627,710,001',
       unstakedAt: '1,648,627,710,001',
     });
-    queryServiceRequestByIdSpy.mockImplementation(() => 
-      Promise.resolve(RESULT)
+    queryServiceRequestByIdSpy.mockImplementation(() =>
+      Promise.resolve(RESULT),
     );
 
     // Act
