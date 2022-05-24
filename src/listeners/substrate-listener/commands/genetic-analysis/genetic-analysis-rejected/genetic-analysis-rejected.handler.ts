dd41cd4060bc687a9afc786d5309d6e70a12053a
rubenkristian <rubenkristian96@gmail.com>
2022-05-24 18:41:00 +0700

    notification customer request reject

--- src/listeners/substrate-listener/commands/genetic-analysis/genetic-analysis-rejected/genetic-analysis-rejected.handler.ts
+++ src/listeners/substrate-listener/commands/genetic-analysis/genetic-analysis-rejected/genetic-analysis-rejected.handler.ts
@@ -1,8 +1,10 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
-import { SubstrateService } from '../../../../../common';
+import { DateTimeProxy, SubstrateService } from '../../../../../common';
 import { setGeneticAnalysisOrderRefunded } from '@debionetwork/polkadot-provider';
 import { GeneticAnalysisRejectedCommand } from './genetic-analysis-rejected.command';
+import { NotificationService } from '../../../../../endpoints/notification/notification.service';
+import { NotificationDto } from '../../../../../endpoints/notification/dto/notification.dto';
 
 @Injectable()
 @CommandHandler(GeneticAnalysisRejectedCommand)
@@ -12,7 +14,11 @@ export class GeneticAnalysisRejectedHandler
   private readonly logger: Logger = new Logger(
     GeneticAnalysisRejectedCommand.name,
   );
-  constructor(private readonly substrateService: SubstrateService) {}
+  constructor(
+    private readonly substrateService: SubstrateService,
+    private readonly dateTimeProxy: DateTimeProxy,
+    private readonly notificationService: NotificationService,
+  ) {}
 
   async execute(command: GeneticAnalysisRejectedCommand) {
     await this.logger.log('Genetic Analysis Rejected!');
@@ -24,6 +30,22 @@ export class GeneticAnalysisRejectedHandler
         this.substrateService.api as any,
         this.substrateService.pair,
         geneticAnalysis.geneticAnalysisOrderId,
+        async () => {
+          const orderRefundedNotification: NotificationDto = {
+            role: 'Customer',
+            entity_type: 'Genetic Analysis Tracking',
+            entity: 'Order Rejected',
+            description: `Your sample from ${geneticAnalysis.geneticAnalysisOrderId} has been rejected. Click here to see your order details.`,
+            read: false,
+            created_at: this.dateTimeProxy.new(),
+            updated_at: this.dateTimeProxy.new(),
+            deleted_at: null,
+            from: 'Debio Network',
+            to: geneticAnalysis.ownerId,
+          };
+
+          this.notificationService.insert(orderRefundedNotification);
+        },
       );
     } catch (error) {
       await this.logger.log(error);
