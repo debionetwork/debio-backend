25a7737723973b90afe0b0ce25898bc8d7abb2b7
Kirisu <56469224+rubenkristian@users.noreply.github.com>
2022-06-07 12:34:36 +0700

    feat: order and payment notifications (#357)
    * Update it.yml
    
    * Update it.yml
    
    * feat: enhance listener and error wasm(#342)
    
    * re new branch
    
    * big fixing API substrate/customer/:customer_id
    
    * fixing endpoint and unit testing
    
    * upgrate package to 0.0.4
    
    * run githooks
    
    * bugFix: fixing failed build
    
    * feat: enhance listener with sync block
    
    * feat: enhance listener with sync block
    
    * fixing module
    
    * feat: set block number to indexer
    
    * feat: set block number to indexer
    
    * feat: add cryptoWaitReady
    
    * feat: add cryptoWaitReady
    
    * enhance: ga order fulfilled service charge logging (#346)
    
    * enhance: enhance listener GA fulfilled for insert loging service charge
    
    * enhance: enhance listener GA fulfilled for insert loging service charge
    
    * feat: listener, unit test for lab & fixing forward payment to escrow when order fulfilled (#344)
    
    * fix: resolve order fulfilled to forward payment smart contract
    
    * feat: listener and logging for lab registered, staked, unstaked, retrieve unstaked listener
    
    * tests: lab registered listener unit test
    
    * tests: unit test lab staked successful listener
    
    * tests: unit test lab unstaked successful
    
    * tests: unit test lab retrieve unstaked amount
    
    * update event for notification lab
    
    * fix message
    
    * update order failed handler and fulfilled

--- src/listeners/substrate-listener/commands/orders/order-failed/order-failed.handler.ts
+++ src/listeners/substrate-listener/commands/orders/order-failed/order-failed.handler.ts
@@ -2,13 +2,15 @@ import { Injectable, Logger } from '@nestjs/common';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { OrderFailedCommand } from './order-failed.command';
 import { EscrowService } from '../../../../../common/modules/escrow/escrow.service';
-import { SubstrateService } from '../../../../../common';
+import { DateTimeProxy, SubstrateService } from '../../../../../common';
 import {
   Order,
   setOrderRefunded,
   finalizeRequest,
   sendRewards,
 } from '@debionetwork/polkadot-provider';
+import { NotificationDto } from '../../../../../endpoints/notification/dto/notification.dto';
+import { NotificationService } from '../../../../../endpoints/notification/notification.service';
 
 @Injectable()
 @CommandHandler(OrderFailedCommand)
@@ -18,6 +20,8 @@ export class OrderFailedHandler implements ICommandHandler<OrderFailedCommand> {
   constructor(
     private readonly escrowService: EscrowService,
     private readonly substrateService: SubstrateService,
+    private readonly notificationService: NotificationService,
+    private readonly dateTimeProxy: DateTimeProxy,
   ) {}
 
   async execute(command: OrderFailedCommand) {
@@ -40,16 +44,35 @@ export class OrderFailedHandler implements ICommandHandler<OrderFailedCommand> {
       this.substrateService.api as any,
       this.substrateService.pair,
       order.id,
+      async () => {
+        // QC notification to lab
+        const labNotification: NotificationDto = {
+          role: 'Lab',
+          entity_type: 'Genetic Testing Order',
+          entity: 'Order Failed',
+          description: `You've received ${order.additionalPrices[0]} DAI as quality control fees for ${order.dnaSampleTrackingId}.`,
+          read: false,
+          created_at: this.dateTimeProxy.new(),
+          updated_at: this.dateTimeProxy.new(),
+          deleted_at: null,
+          from: 'Debio Network',
+          to: order.sellerId,
+        };
+
+        await this.notificationService.insert(labNotification);
+      },
     );
   }
 
   callbackSendReward(order: Order): void {
+    const rewardCustomer = +order.additionalPrices[0].value * 10 ** 18;
+    const rewardLab = rewardCustomer / 10;
     //send reward for customer
     sendRewards(
       this.substrateService.api as any,
       this.substrateService.pair,
       order.customerId,
-      (+order.additionalPrices[0].value * 10 ** 18).toString(),
+      rewardCustomer.toString(),
     );
 
     //send reward for customer
@@ -57,7 +80,7 @@ export class OrderFailedHandler implements ICommandHandler<OrderFailedCommand> {
       this.substrateService.api as any,
       this.substrateService.pair,
       order.sellerId,
-      ((+order.additionalPrices[0].value * 10 ** 18) / 10).toString(),
+      rewardLab.toString(),
     );
   }
 }
