f2e2b8934b1ba650e72f1452754ef6b88150e9ee
Kirisu <56469224+rubenkristian@users.noreply.github.com>
2022-06-08 08:37:57 +0700

    feat: genetic analyst notification add new service (#379)
    * Update it.yml
    
    * Update it.yml
    
    * feat: enhance listener and error wasm(#342)
    
    * re new branch
    
    * big fixing API substrate/customer/:customer_id
    
    * fixing endpoint and unit testing
    
    * upgrate package to 0.0.4
    
    * run githooks
    
    * bugFix: fixing failed build
    
    * feat: enhance listener with sync block
    
    * feat: enhance listener with sync block
    
    * fixing module
    
    * feat: set block number to indexer
    
    * feat: set block number to indexer
    
    * feat: add cryptoWaitReady
    
    * feat: add cryptoWaitReady
    
    * enhance: ga order fulfilled service charge logging (#346)
    
    * enhance: enhance listener GA fulfilled for insert loging service charge
    
    * enhance: enhance listener GA fulfilled for insert loging service charge
    
    * feat: listener, unit test for lab & fixing forward payment to escrow when order fulfilled (#344)
    
    * fix: resolve order fulfilled to forward payment smart contract
    
    * feat: listener and logging for lab registered, staked, unstaked, retrieve unstaked listener
    
    * tests: lab registered listener unit test
    
    * tests: unit test lab staked successful listener
    
    * tests: unit test lab unstaked successful
    
    * tests: unit test lab retrieve unstaked amount
    
    * update polkadot-povider 0.0.11 and notification genetic analyst service created
    
    Co-authored-by: Agustinus Theodorus <37565691+agustinustheo@users.noreply.github.com>
    Co-authored-by: JackyRahman <78291196+JackyRahman@users.noreply.github.com>

---
+++ src/listeners/substrate-listener/commands/genetic-analyst-services/genetic-analyst-service-created/genetic-analyst-service-created.handler.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { DateTimeProxy } from '../../../../../common';
+import { NotificationService } from '../../../../../endpoints/notification/notification.service';
+import { GeneticAnalystServiceCreatedCommand } from './genetic-analyst-service-created.command';
+import { NotificationDto } from '../../../../../endpoints/notification/dto/notification.dto';
+
+@Injectable()
+@CommandHandler(GeneticAnalystServiceCreatedCommand)
+export class GeneticAnalystServiceCreatedCommandHandler
+  implements ICommandHandler<GeneticAnalystServiceCreatedCommand>
+{
+  constructor(
+    private readonly notificationService: NotificationService,
+    private readonly dateTimeProxy: DateTimeProxy,
+  ) {}
+
+  async execute(command: GeneticAnalystServiceCreatedCommand) {
+    const geneticAnalystService = command.geneticAnalystService;
+
+    const geneticAnalystServiceNotification: NotificationDto = {
+      role: 'GA',
+      entity_type: 'Genetic Analyst',
+      entity: 'Add service',
+      description: `You've successfully added your new service - ${geneticAnalystService.info.name}.`,
+      read: false,
+      created_at: this.dateTimeProxy.new(),
+      updated_at: this.dateTimeProxy.new(),
+      deleted_at: null,
+      from: 'Debio Network',
+      to: geneticAnalystService.ownerId,
+    };
+
+    this.notificationService.insert(geneticAnalystServiceNotification);
+  }
+}
