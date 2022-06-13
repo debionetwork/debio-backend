f3a6af2f0cf30fb516e6aaae8dc3553f477fb50f
unknown <jackyrahman14@gmail.com>
2022-06-13 09:32:01 +0700

    feat: add notification insert Request Analysis Submitted and unit test

--- src/listeners/substrate-listener/commands/genetic-analysis-order/genetic-analysys-order-created/genetic-analysis-order-created.handler.ts
+++ src/listeners/substrate-listener/commands/genetic-analysis-order/genetic-analysys-order-created/genetic-analysis-order-created.handler.ts
@@ -1,8 +1,10 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { TransactionLoggingDto } from '../../../../../common/modules/transaction-logging/dto/transaction-logging.dto';
-import { TransactionLoggingService } from '../../../../../common';
+import { TransactionLoggingService, DateTimeProxy } from '../../../../../common';
 import { GeneticAnalysisOrderCreatedCommand } from './genetic-analysis-order-created.command';
+import { NotificationService } from '../../../../../endpoints/notification/notification.service';
+import { NotificationDto } from '../../../../../endpoints/notification/dto/notification.dto';
 
 @Injectable()
 @CommandHandler(GeneticAnalysisOrderCreatedCommand)
@@ -12,7 +14,11 @@ export class GeneticAnalysisOrderCreatedHandler
   private readonly logger: Logger = new Logger(
     GeneticAnalysisOrderCreatedCommand.name,
   );
-  constructor(private readonly loggingService: TransactionLoggingService) {}
+  constructor(
+    private readonly loggingService: TransactionLoggingService,
+    private readonly notificationService: NotificationService,
+    private readonly dateTimeProxy: DateTimeProxy,
+    ) {}
 
   async execute(command: GeneticAnalysisOrderCreatedCommand) {
     const geneticAnalysisOrder = command.geneticAnalysisOrders.normalize();
@@ -38,8 +44,22 @@ export class GeneticAnalysisOrderCreatedHandler
         transaction_type: 3,
       };
 
+      const notificationInput: NotificationDto = {
+        role: 'Customer',
+        entity_type: 'Genetic Analysis Orders',
+        entity: 'Order Created',
+        description: `Congrats! You have been submitted your account verification.`,
+        read: false,
+        created_at: await this.dateTimeProxy.new(),
+        updated_at: await this.dateTimeProxy.new(),
+        deleted_at: null,
+        from: geneticAnalysisOrder.id,
+        to: 'Debio Network',
+      };
+
       if (!isGeneticAnalysisOrderHasBeenInsert) {
         await this.loggingService.create(geneticAnalysisOrderLogging);
+        await this.notificationService.insert(notificationInput)
       }
     } catch (error) {
       await this.logger.log(error);
