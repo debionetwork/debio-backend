074fbd07150e45c9950d5923104acb5f71563248
unknown <jackyrahman14@gmail.com>
2022-06-29 09:27:04 +0700

    test: e2e lab staking successfull

---
+++ test/e2e/listeners/substrate-listener/commands/labs/stake-successful.spec.ts
@@ -0,0 +1,158 @@
+import { ApiPromise } from '@polkadot/api';
+import 'regenerator-runtime/runtime';
+import { stakeLab } from '@debionetwork/polkadot-provider/lib/command/labs';
+import { queryLabById } from '@debionetwork/polkadot-provider/lib/query/labs';
+import { Lab } from '@debionetwork/polkadot-provider/lib/models/labs';
+import { registerLab } from '@debionetwork/polkadot-provider/lib/command/labs';
+import { labDataMock } from '../../../../../mocks/models/labs/labs.mock';
+import { Order } from '@debionetwork/polkadot-provider/lib/models/labs/orders';
+import { TestingModule } from '@nestjs/testing/testing-module';
+import { Test } from '@nestjs/testing/test';
+import { INestApplication } from '@nestjs/common/interfaces/nest-application.interface';
+import { initializeApi } from '../../../../polkadot-init';
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
+import { LabRating } from '../../../../../../src/endpoints/rating/models/rating.entity';
+import { TransactionRequest } from '../../../../../../src/common/modules/transaction-logging/models/transaction-request.entity';
+import { LocationEntities } from '../../../../../../src/endpoints/location/models';
+import { dummyCredentials } from '../../../../config';
+import { EscrowService } from '../../../../../../src/common/modules/escrow/escrow.service';
+import { escrowServiceMockFactory } from '../../../../../unit/mock';
+import {
+  DateTimeModule,
+  DebioConversionModule,
+  MailModule,
+  NotificationModule,
+  ProcessEnvModule,
+  SubstrateModule,
+  TransactionLoggingModule,
+} from '../../../../../../src/common';
+import { LocationModule } from '../../../../../../src/endpoints/location/location.module';
+import { CqrsModule } from '@nestjs/cqrs';
+import { ElasticsearchModule } from '@nestjs/elasticsearch';
+import { SubstrateListenerHandler } from '../../../../../../src/listeners/substrate-listener/substrate-listener.handler';
+import { createConnection } from 'typeorm';
+import { GeneticTestingCommandHandlers } from '../../../../../../src/listeners/substrate-listener/commands/genetic-testing';
+
+describe('Data Staked Integration Tests', () => {
+  let app: INestApplication;
+
+  let api: ApiPromise;
+  let pair: any;
+  let lab: Lab;
+  let order: Order;
+
+  global.console = {
+    ...console,
+    log: jest.fn(),
+    debug: jest.fn(),
+    info: jest.fn(),
+    warn: jest.fn(),
+    error: jest.fn(),
+  };
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          ...dummyCredentials,
+          database: 'db_postgres',
+          entities: [LabRating, TransactionRequest],
+          autoLoadEntities: true,
+        }),
+        TypeOrmModule.forRoot({
+          name: 'dbLocation',
+          ...dummyCredentials,
+          database: 'db_postgres',
+          entities: [...LocationEntities],
+          autoLoadEntities: true,
+        }),
+        ProcessEnvModule,
+        LocationModule,
+        TransactionLoggingModule,
+        SubstrateModule,
+        DebioConversionModule,
+        MailModule,
+        CqrsModule,
+        DateTimeModule,
+        NotificationModule,
+        ElasticsearchModule.registerAsync({
+          useFactory: async () => ({
+            node: process.env.ELASTICSEARCH_NODE,
+            auth: {
+              username: process.env.ELASTICSEARCH_USERNAME,
+              password: process.env.ELASTICSEARCH_PASSWORD,
+            },
+          }),
+        }),
+      ],
+      providers: [
+        {
+          provide: EscrowService,
+          useFactory: escrowServiceMockFactory,
+        },
+        SubstrateListenerHandler,
+        ...GeneticTestingCommandHandlers,
+      ],
+    }).compile();
+
+    app = module.createNestApplication();
+    await app.init();
+
+    const { api: _api, pair: _pair } = await initializeApi();
+    api = _api;
+    pair = _pair;
+  }, 360000);
+
+  afterAll(() => {
+    api.disconnect();
+  });
+
+  it('lab staking event', async () => {
+    // eslint-disable-next-line
+    const labPromise: Promise<Lab> = new Promise((resolve, reject) => {
+      registerLab(api, pair, labDataMock.info, () => {
+        queryLabById(api, pair.address).then((res) => {
+          resolve(res);
+        });
+      });
+    });
+
+    lab = await labPromise;
+    expect(lab.stakeStatus).toEqual('Unstaked');
+
+    // eslint-disable-next-line
+    const stakedLabPromise: Promise<Lab> = new Promise((resolve, reject) => {
+      stakeLab(api, pair, () => {
+        queryLabById(api, pair.address).then((res) => {
+          resolve(res);
+        });
+      });
+    });
+
+    lab = await stakedLabPromise;
+    expect(lab.stakeStatus).toEqual('Staked');
+
+    const dbConnection = await createConnection({
+      ...dummyCredentials,
+      database: 'db_postgres',
+      entities: [TransactionRequest],
+      synchronize: true,
+    });
+
+    const transactionLogs = await dbConnection
+      .getRepository(TransactionRequest)
+      .createQueryBuilder('transaction_logs')
+      .where('transaction_logs.transaction_type = :transaction_type', {
+        transaction_type: 6,
+      })
+      .where('transaction_logs.transaction_status = :transaction_status', {
+        transaction_status: 26,
+      })
+      .getMany();
+
+    expect(transactionLogs[0].ref_number).toEqual(order.id);
+    expect(transactionLogs[0].transaction_type).toEqual(6);
+    expect(transactionLogs[0].transaction_status).toEqual(26);
+  }, 180000);
+});
