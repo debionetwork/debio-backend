0a63e48b1b33851b3e011c69d6e046d051421830
unknown <jackyrahman14@gmail.com>
2022-07-01 10:00:51 +0700

    test: change queue data stake from middle to end queue

--- test/e2e/listener-test-sequencer.js
+++ test/e2e/listener-test-sequencer.js
@@ -12,9 +12,6 @@ class CustomSequencer extends Sequencer {
       return obj.path.includes('listeners/substrate-listener/commands/orders/order-fulfilled.spec.ts');
     }));
     sequence.push(...testsArray.filter(obj => {
-      return obj.path.includes('listeners/substrate-listener/commands/genetic-testing/data-staked.spec.ts');
-    }));
-    sequence.push(...testsArray.filter(obj => {
       return obj.path.includes('listeners/substrate-listener/commands/service-request/service-request-partial.spec.ts');
     }));
     sequence.push(...testsArray.filter(obj => {
@@ -25,6 +22,9 @@ class CustomSequencer extends Sequencer {
     }));
     sequence.push(...testsArray.filter(obj => {
       return obj.path.includes('listeners/substrate-listener/commands/genetic-analysis-order/genetic-analysis-order-created.spec.ts');
+    }));
+    sequence.push(...testsArray.filter(obj => {
+      return obj.path.includes('listeners/substrate-listener/commands/genetic-testing/data-staked.spec.ts');
     }));
 
     return sequence;
