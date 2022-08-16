eec898ac94c517127f73534da256fb98766d8deb
rubenkristian <rubenkristian96@gmail.com>
2022-08-16 12:27:25 +0700

    rename transaction type table

--- src/common/modules/transaction-type/models/transaction-type.entity.ts
+++ src/common/modules/transaction-type/models/transaction-type.entity.ts
@@ -1,7 +1,7 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { TransactionTypeList } from './transaction-type.list';
 
-@Entity({ name: 'transaction_type' })
+@Entity({ name: 'transaction_types' })
 export class TransactionType {
   @PrimaryGeneratedColumn()
   id: number;
