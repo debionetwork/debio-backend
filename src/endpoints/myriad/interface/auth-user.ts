f8212e2dac5193d52b4e74ca2ea410f40eeb409a
Kristian Ruben <56469224+rubenkristian@users.noreply.github.com>
2023-02-23 23:24:49 +0700

    fix: update auth login myriad (#592)
    - adjustment response from myriad api auth (changed from myriad)

--- src/endpoints/myriad/interface/auth-user.ts
+++ src/endpoints/myriad/interface/auth-user.ts
@@ -1,3 +1,15 @@
-export interface AuthUserInterface {
+export interface UserInterface {
+  id: string;
+  email: string;
+  username: string;
+  address: string;
+}
+
+export interface TokenInterface {
   accessToken: string;
+}
+
+export interface AuthUserInterface {
+  user: UserInterface;
+  token: TokenInterface;
 }
